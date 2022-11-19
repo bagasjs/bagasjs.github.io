f0a093a98f3f2fe2d488d76d3fef7b35894740f8
Nakano Yoichi <nakanoyoichi83@gmail.com>
2022-11-20 05:27:29 +0700

    Fixing index.html and the merge conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,4 @@ if(Typed)
 		typeSpeed : speed,
 		backSpeed : speed,
 	});
-=======
-if(Typed)
-{
-	const speed = 1;
-	const typed = new Typed(".messages", {
-		strings : [
-			"",
-
-		],
-		loop : true,
-		typeSpeed : speed,
-		backSpeed : speed,
-	});
 }
